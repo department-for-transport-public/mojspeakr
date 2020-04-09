82f523133608ce809f9510c4392708f6032fb75c
BrydenFrancesca <brydenfrancesca@gmail.com>
2020-04-09 10:57:17 +0000

    Made some functions internal only

--- R/HTML_functions.R
+++ R/HTML_functions.R
@@ -31,25 +31,26 @@ bold_key_words <- function(data, key_words = NULL) {
 #'   (!!n). The filename must start and end with a number and have text in
 #'   between (eg, "1-abcd-1.png")
 #' @name generate_image_references
+#' @keywords internal
 #' @title Generate govdown image references
 generate_image_references <- function(img_filenames) {
   image_references <- data.frame(image_file = img_filenames)
   # Strip ext - not image file specific
   image_references$image_reference <- tools::file_path_sans_ext(image_references$image_file)
-  
+
   # Capture chunk number and image position within chunk
   if (!all(as.logical(lapply(image_references$image_file, stringr::str_detect, "^[0-9]")))) {
     stop("image chunk names must start with a number, which should correspond to their order in the .Rmd file")
   }
   image_references$pre_dec <- gsub("([0-9]+).*$", "\\1", image_references$image_reference)
   image_references$post_dec <- gsub(".*([0-9]+)$", "\\1", image_references$image_reference)
-  
+
   # Convert to decimal for ranking
   image_references$combined <- as.numeric(paste0(image_references$pre_dec,
                                                  ".",
                                                  image_references$post_dec))
   image_references$image_reference <- paste0("!!", rank(image_references$combined))
-  
+
   # Keep mapping of image files to govspeak references
   image_references <- image_references[, c("image_file", "image_reference")]
   return(image_references)
@@ -62,22 +63,23 @@ generate_image_references <- function(img_filenames) {
 #' @param md_file string with markdown file text
 #' @param images_folder string; folder containing images for *.md file
 #' @name convert_image_references
+#' @keywords internal
 #' @title Convert markdown image references to govdown
 convert_image_references <- function(image_references, md_file, images_folder) {
   govspeak_image_reference_file <- as.character(md_file)
   for (i in seq_along(image_references$image_file)) {
     file_name <- image_references$image_file[i]
-    
+
     # Construct markdown reference to image file
     md_image_format <- paste0("!\\[\\]\\(",
                               images_folder,
                               "/",
                               file_name,
                               "\\)<!-- -->")
-    
+
     govspeak_reference <- paste0(as.character(image_references$image_reference[i]),
                                  "\n")
-    
+
     # Replace markdown image reference with govspeak reference
     govspeak_image_reference_file <- gsub(md_image_format,
                                           govspeak_reference,
@@ -90,6 +92,7 @@ convert_image_references <- function(image_references, md_file, images_folder) {
 #' Convert markdown callout sytax to govspeak
 #' @param md_file string; markdown file text
 #' @name convert_callouts
+#' @keywords internal
 #' @title Convert markdown callout syntax to govspeak
 convert_callouts <- function(md_file) {
   # Lazy match on lines starting with ">", which are then flanked with "^"
@@ -101,6 +104,7 @@ convert_callouts <- function(md_file) {
 #' Replace R markdown header with title only
 #' @param md_file string; markdown file text
 #' @name remove_header
+#' @keywords internal
 #' @title Replace R markdown header with ## title
 remove_header <- function(md_file) {
   # Lazy match on header to extract title
@@ -113,6 +117,7 @@ remove_header <- function(md_file) {
 #' Remove R markdown multiline block elements (package warnings, but also multiline code blocks)
 #' @param md_file string; markdown file text
 #' @name remove_rmd_blocks
+#' @keywords internal
 #' @title Remove R markdown multiline block elements (package warning and code block)
 remove_rmd_blocks <- function(md_file) {
   # Lazy match on warnings and code blocks
@@ -134,29 +139,29 @@ convert_rmd <- function(path,
                         remove_blocks=FALSE
 ) {
   md_file <- paste(readLines(path), collapse = "\n")
-  
+
   img_files <- list.files(paste0(dirname(path),
                                  "/",
                                  images_folder))
-  
+
   image_references <- generate_image_references(img_files)
   govspeak_file <- convert_image_references(image_references,
                                             md_file,
                                             images_folder)
-  
+
   govspeak_file <- remove_header(govspeak_file)
-  
+
   govspeak_file <- convert_callouts(govspeak_file)
-  
+
   ##Remove long strings of hashes and move all headers down one level
   govspeak_file <- gsub("#####", "-----", govspeak_file)
   govspeak_file <- gsub('#(?=[A-Z])', '\\1# ', govspeak_file, perl=T)
   govspeak_file <- gsub('# (?=[A-Z])', '\\1## ', govspeak_file, perl=T)
-  
+
   if (remove_blocks) {
     govspeak_file <- remove_rmd_blocks(govspeak_file)
   }
-  
+
   write(govspeak_file, gsub("\\.md", "_converted\\.md", path))
 }
 
