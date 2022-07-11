028ad1b767362475b37bb743e3f7764facfeae07
Francesca Bryden <brydenfrancesca@gmail.com>
2022-07-11 12:36:37 +0100

    Removed requirement for asis call in chunk

--- R/callout_box.R
+++ R/callout_box.R
@@ -5,13 +5,10 @@
 #' @name callout_box
 #' @title Include a callout box in govspeak output
 callout_box <- function(text, format = "html"){
-  ##If format of chunk is not set to "asis", return a warning
-  if(opts_current$get("results") != "asis"){
-    warning("Chunk option must be set to result = 'asis'")
-  }
-
   if(knitr::opts_knit$get("rmarkdown.pandoc.to") %in% format){
-    cat(paste("$CTA", text, "$CTA", sep = '\n'))
+
+      knitr::asis_output(paste("$CTA", text, "$CTA", sep = '\n'))
+
   } else{
     cat(paste(text))}
 }
