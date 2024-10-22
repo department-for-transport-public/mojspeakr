aaa9d50d6d83a2a35efec63ca044bffb3ebc9a6c
Francesca Bryden <francesca.bryden@dft.gov.uk>
2024-10-22 16:05:15 +0000

    Clean up obsolete code

--- R/convert_rmd.R
+++ R/convert_rmd.R
@@ -14,38 +14,25 @@
 #' If "line", page breaks are replaced with a horizontal rule. If "none"
 #' they are replaced with a line break.
 #' If "unchanged" they are not removed.
-#' @param img_wd path to working directory where images folder is stored.
-#' In normal usage, this will be the same folder as your markdown file is saved
-#' in. This can be passed as an absolute or relative path. Providing a path
-#' will convert your image names into a standardised form, default is NULL
-#' which does not convert image names.
 #' @export
-#' @importFrom utils write.csv
 #' @name convert_rmd
 #' @title Convert standard markdown file to govspeak
 convert_rmd <- function(path,
                         remove_blocks = FALSE,
                         sub_pattern = TRUE,
-                        page_break = "line",
-                        img_wd = NULL) {
+                        page_break = "line") {
 
+  ##Check the path is a .md format
+  if(!grep("[.]md$", path)){
+    stop("Please provide a .md format file.
+         Note that this function does not work directly on the .Rmd rmarkdown document.")
+  }
 
   #Read in the raw lines from the md
   md_file <- readLines(path)
 
-  #Get a nice table of image references
-  img_ref <- generate_image_references(md_file)
-
-  ##Check there are actually some image references
-  if(!is.null(img_ref)){
-    #Lets do a little gsub
-    for (i in 1:nrow(img_ref)) {
-      md_file <- gsub(paste0(img_ref[i, "img_tags"], "<!-- -->"),
-                      paste0(img_ref[i, "govspeak"], "\n"),
-                      md_file,
-                      fixed = TRUE)
-    }
-  }
+  #Nicely format the image references
+  md_file <- generate_image_references(md_file)
 
   ##Turn md file into plain text
   govspeak_file <- paste(md_file, collapse = "\n")
@@ -84,16 +71,5 @@ convert_rmd <- function(path,
     message("File converted successfully")
   }
 
-  ##If img_wd isn't set to NULL, convert our image tags
-  if(!is.null(img_wd)){
-    img_ref <- order_img_ref(img_ref = img_ref,
-                             img_wd = img_wd)
-  }
-  if(!is.null(img_ref)){
-  ##Write out lil csv of the image path to govspeak lookup
-  write.csv(img_ref[, c("img_ref", "govspeak")],
-            file = paste0(gsub("(.*[/])*.", "\\1", path), "img_lookup.csv"),
-            row.names = FALSE)
-  }
 
 }
--- R/internal_functions.R
+++ R/internal_functions.R
@@ -1,76 +1,17 @@
-#' Compares summary points to vector of words indicating change,
-#'  and highlights relevant words in bold.
-#' @param data character vector of summary points
-#' @param key_words character vector of additional words you would like
-#' to highlight in bold (defaults to NULL)
-#' @param key_words_remove character vector of standard key words you would NOT
-#' like to highlight in bold (defaults to NULL)
-#' @name bold_key_words
-#' @title Highlight in bold key words in summary text
-bold_key_words <- function(data, key_words = NULL, key_words_remove = NULL) {
-
-  ##Concatenate chosen key words with pre-existing list
-  key_words <- c(key_words, "decreasing", "decreased", "down", "lower", "fall",
-                 "decrease", "fell", "increasing", "increased", "up", "risen",
-                 "increase", "rose", "stable", "no change")
-
-  #If key words to remove is not NULL, remove listed words from key_words
-  if (!is.null(key_words_remove)) {
-    key_words <-  key_words[!(key_words %in% key_words_remove)]
-  }
-
-  temp <- data
-  for (i in 1:length(key_words)) {
-
-    temp <- gsub(paste0("\\b", key_words[i], "\\b"),
-                 paste0("**", key_words[i], "**"),
-                 temp,
-                 ignore.case = TRUE)
-
-  }
-  return(temp)
-}
-
-
-
-#' Extracts image references from the original markdown output
-#' creates a lookup between filename and new govdown reference number
+#' Extracts image references from the original markdown output into Whitehall format
 #' @param lines raw contents of md file (from readLines)
 #' @name generate_image_references
 #' @keywords internal
 #' @title Generate govdown image references
 generate_image_references <- function(lines) {
 
-  ##Find the image tags (one per line chaps)
-  img_tags <- lines[grep("\\!\\[\\]\\(*.", lines)]
-  ##Stop at this point if there aren't any
-  if(length(img_tags) == 0){
-    message("No images found")
-  } else{
-  #Split on the ! symbols
-  img_tags <- unlist(strsplit(img_tags, split = "(?<=.)(?=[!])", perl = TRUE))
-  ##Drop anything which doesn't then start with ![]
-  img_tags <- img_tags[grepl('![]', img_tags, fixed = TRUE)]
-  #Clean up trailing < symbols
-  img_tags <- gsub("[<]$", "", img_tags)
+  #Split any lines on <-- --> to get one image per line chaps
+  lines <- unlist(strsplit(lines, "<!-- -->"))
 
-  ##Make it a table with row line numbers from original text indicated
-  data <- data.frame(img_tags = img_tags)
+  ##Sub out markdown format for Whitehall format
+  lines <- gsub("\\!\\[\\]\\(.*\\/(.*)\\)", "[Image:\\1]", lines)
 
-  for (i in 1:nrow(data)){
-    data[i, "lines"] <- grep(data[i, "img_tags"], lines, fixed = TRUE)
-  }
-
-  ##Order by row number and figure name
-  #(will be in order if they're from the same block)
-  data <- data[order(data$lines, data$img_tags), ]
-
-  ##Create clean image references and govspeak tags
-  data$img_ref <- gsub("\\!\\[\\][(](.*)[)]", "\\1", data$img_tags)
-  data$govspeak <- paste0("!!", row.names(data))
-
-  return(data)
-  }
+  return(lines)
 }
 
 
@@ -154,44 +95,4 @@ hash_sub <- function(x, sub_type) {
 
     return(x)
   }
-}
-
-#Take our image references and number them in a way that
-#doesn't look like a drunk mathematician rolling dice
-#' @param img_ref table of image references
-#' @param img_wd working directory that image paths are listed from.
-#' In most cases, this will be the same folder as the md document is stored in
-#' @name order_img_ref
-#' @keywords internal
-#' @title Order image references in a logical way
-
-order_img_ref <- function(img_ref, img_wd){
-
-  ##Take image references and swap out the img_ref so they're all just called
-  #image-something in order
-  img_ref$img_ref_old <- img_ref$img_ref
-
-  for(i in 1:nrow(img_ref)){
-    img_ref[i, "img_ref"] <- gsub("(.*[/]).*([.].*)$",
-                                  paste0("\\1", "image", i, "\\2"),
-                                  img_ref[i, "img_ref"])
-
-    #Rename our images to match
-    #Include warning if it's not found
-    if(file.exists(file.path(img_wd, img_ref[i, "img_ref_old"]))){
-      file.rename(
-        from = file.path(img_wd, img_ref[i, "img_ref_old"]),
-        to = file.path(img_wd, img_ref[i, "img_ref"]))
-
-      message(file.path(img_wd, img_ref[i, "img_ref_old"]), " renamed to ",
-      file.path(img_wd, img_ref[i, "img_ref"]))
-    } else{
-
-      warning(
-       file.path(img_wd, img_ref[i, "img_ref_old"]), " not found")
-    }
-  }
-
-  return(img_ref)
-
 }
