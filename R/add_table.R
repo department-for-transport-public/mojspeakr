4bc8fc9f635df9038b750a9293d57bf213334ead
Francesca Bryden <francesca.bryden@dft.gov.uk>
2024-10-23 08:37:07 +0000

    Updated add_table function with improved documentation and usage

--- R/add_table.R
+++ R/add_table.R
@@ -1,9 +1,23 @@
 #' Add a table in Rmarkdown suitable for publication in govspeak and in word/PDF
 #' @param data dataframe; dataframe of any size to be displayed as a table
+#' @param format.args additional format arguments to be passed to the table.
+#' Defaults to comma thousands separation
 #' @export
 #' @name add_table
+#' @returns a markdown formatted table as a string
+#' @examples
+#' add_table(mtcars)
+#' ##Example with comma formatted values
+#' add_table(datasets::EuStockMarkets)
+#' ##Example with format.args set to null
+#' add_table(datasets::EuStockMarkets, format.args = NULL)
+#'
 #' @importFrom knitr kable
-#' @title Format data in a table suitable for publication
-add_table <- function(data) {
-  knitr::kable(data, format = "markdown")
+#' @title Format data in a markdown table suitable for publication, with comma separation on numbers
+
+add_table <- function(data, format.args = list(big.mark = ",")) {
+  knitr::kable(data,
+               format = "markdown",
+               format.args = format.args
+               )
 }
